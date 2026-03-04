07bc1cb9c1758422d32eb0f376b62688e1f5d1e4
mbilarus <maxim.bilarus@gmail.com>
2026-03-04 19:16:12 +0300

    removed flat data

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,3 @@
-pub mod flat_data;
 pub mod sortable;
 
-pub use flat_data::FlatData;
 pub use sortable::Sortable;
--- src/models/flat_data.rs
+++
@@ -1,8 +0,0 @@
-use super::sortable::Sortable;
-
-#[repr(C)]
-pub struct FlatData {
-    pub values: *const Sortable,
-    pub len: usize,
-    pub width: usize,
-}
